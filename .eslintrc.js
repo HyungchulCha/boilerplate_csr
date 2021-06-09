563365aa838b48678b6f96e56fb2e77553999797
HyunchchulCha <ckgudcjf@gmail.com>
2021-06-09 17:53:31 +0900

    eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  plugins: ['prettier'],
+  extends: ['react-app', 'airbnb', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:prettier/recommended'],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extensions': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+  },
+};
